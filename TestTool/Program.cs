b99cb86d9fff241df99cbb58dc3628508a20b24f
Ceferino Torres Villa <ceferino.torres.tic@gmail.com>
2016-04-05 17:25:07 -0500

    Windows Console project added for testing.
    Signed-off-by: Ceferino Torres Villa <ceferino.torres.tic@gmail.com>

---
+++ TestTool/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Tools;
+
+namespace TestTool {
+    class Program {
+        static void Main ( string [ ] args ) {
+            Logger.getInstance ( "Tools" ).d ( "Main", "hola mundo" );
+            Console.ReadKey ();
+        }
+    }
+}
